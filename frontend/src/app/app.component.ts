6c6cab05f1e8c0288595b63dc62d91b6560539e8
Kinan <Kinan@company.com>
2024-08-04 20:25:02 +0300

    Add scrollable hamburger menu
    -The menu will let user to navigate between different pages like cart or
    order components
    -Add styles and import relevant angular built-in modules of Sidenav toolbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatDialogModule, MatSidenavModule, MatListModule, MatToolbarModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
