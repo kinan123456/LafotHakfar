559ea6ea47928368db40e9a667f1fd4ec5b1d7c0
Kinan <Kinan@company.com>
2024-08-04 18:55:33 +0300

    Add OrderComponent to routes array

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+import { OrderComponent } from './components/order/order.component';
 
 export const routes: Routes = [
+    { path: 'orders', component: OrderComponent },
     { path: '', redirectTo: '/orders', pathMatch: 'full' },
  ];
