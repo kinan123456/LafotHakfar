b8e387fb8dbcfd1649db3efe1eb5ec6aa8a1c946
Kinan <Kinan@company.com>
2025-02-19 23:22:35 +0200

    Import RouterModule to make toolbar navigation work

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,12 +4,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatDialogModule, MatSidenavModule, MatListModule, MatToolbarModule, MatIconModule],
+  imports: [RouterOutlet, RouterModule, MatDialogModule, MatSidenavModule, MatListModule, MatToolbarModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
