78360d01fbf32429bf226ae996169ece0b78fed0
Kinan <Kinan@company.com>
2025-02-23 15:26:06 +0200

    Remove fake image URL of Bread from OrderService

--- frontend/src/app/services/order.service.ts
+++ frontend/src/app/services/order.service.ts
@@ -11,62 +11,38 @@ export class OrderService {
             {
                 id: '1',
                 name: 'White Bread',
-                image: 'assets/images/white-bread.jpg',
+                image: '',
                 price: 5.00
             },
             {
                 id: '2',
                 name: 'Whole Wheat Bread',
-                image: 'assets/images/whole-wheat-bread.jpg',
+                image: '',
                 price: 6.00
             },
             {
                 id: '3',
                 name: 'Sourdough Bread',
-                image: 'assets/images/sourdough-bread.jpg',
+                image: '',
                 price: 7.50
             },
             {
                 id: '4',
                 name: 'Multigrain Bread',
-                image: 'assets/images/multigrain-bread.jpg',
+                image: '',
                 price: 6.75
             },
             {
                 id: '5',
                 name: 'Rye Bread',
-                image: 'assets/images/rye-bread.jpg',
+                image: '',
                 price: 6.50
             },
             {
                 id: '6',
                 name: 'Ciabatta Bread',
-                image: 'assets/images/ciabatta-bread.jpg',
+                image: '',
                 price: 8.00
-            },
-            {
-                id: '7',
-                name: 'Focaccia Bread',
-                image: 'assets/images/focaccia-bread.jpg',
-                price: 9.00
-            },
-            {
-                id: '8',
-                name: 'Baguette',
-                image: 'assets/images/baguette.jpg',
-                price: 4.50
-            },
-            {
-                id: '9',
-                name: 'Brioche',
-                image: 'assets/images/brioche.jpg',
-                price: 7.00
-            },
-            {
-                id: '10',
-                name: 'Gluten-Free Bread',
-                image: 'assets/images/gluten-free-bread.jpg',
-                price: 8.50
             }
         ];
 
